159b40fad7a3218909685078c1d73645dd79b994
nameisjayant <jayntkumar99gmail.com>
2022-06-14 09:04:00 +0530

    Initial commit

---
+++ app/src/main/java/com/yt/navigationcomponent/screens/FirstScreen.kt
@@ -0,0 +1,30 @@
+package com.yt.navigationcomponent.screens
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class FirstScreen : Screen {
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Button(onClick = {
+            navigator.push(SecondScreen())
+        }) {
+            Text(text = "Click here")
+        }
+    }
+}
+
+class SecondScreen : Screen{
+
+    @Composable
+    override fun Content() {
+        Text(text = "Second screen")
+    }
+
+}
